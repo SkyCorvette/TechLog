43af04ebfa0c4ad69b535dbd177746c7abf2f5da
Igor Karvetsky <igor.karvetsky@gmail.com>
2017-05-25 18:39:49 +0300

    try

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,43 @@ namespace po = boost::program_options;
 
 int main(int argc, const char** argv)
 {
-    po::options_description all("Allowed options");
-
-    po::options_description regexp("Regexp selection and interpretation");
-    regexp.add_options()("ignore-case,i", "ignore case distinctions");
-    all.add(regexp);
-
-    po::options_description misc("Miscellaneous");
-    misc.add_options()("help", "display this help text and exit");
-    all.add(misc);
-
-    po::options_description output("Output control");
-    output.add_options()("no-filename,h", "suppress the file name prefix on output");
-    all.add(output);
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, all), vm);
-    po::notify(vm);
-
-    if (vm.count("help"))
+    try
     {
-        std::cout << all << "\n";
-        return 1;
+        po::options_description all("Allowed options");
+
+        po::options_description regexp("Regexp selection and interpretation");
+        regexp.add_options()("ignore-case,i", "ignore case distinctions");
+        all.add(regexp);
+
+        po::options_description misc("Miscellaneous");
+        misc.add_options()("help", "display this help text and exit");
+        all.add(misc);
+
+        po::options_description output("Output control");
+        output.add_options()("no-filename,h", "suppress the file name prefix on output");
+        all.add(output);
+
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, all), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << all << std::endl;
+            return 1;
+        }
+
+        if (vm.count("ignore-case")) {
+            std::cout << "ignore-case" << std::endl;
+        }
+
+        if (vm.count("no-filename")) {
+            std::cout << "no-filename" << std::endl;
+        }
     }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 1;
 }
