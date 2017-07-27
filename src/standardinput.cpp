6f221637b5cbff778ff879ee6d6460689d297b2c
SkyCorvette <igor.karvetsky@gmail.com>
2017-07-27 05:50:44 +0300

    Delete PVS-Studio comment.

--- src/standardinput.cpp
+++ src/standardinput.cpp
@@ -1,6 +1,3 @@
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-
 #include "standardinput.h"
 
 ssize_t StandardInput::readNext(char *buffer, size_t count)
